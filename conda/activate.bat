bf848b9d53a3248c0f1e31ef95737b4be837e525
Alexander Clouter <alex@digriz.org.uk>
2018-09-06 18:54:15 +0100

    remove quotes

--- conda/activate.bat
+++ conda/activate.bat
@@ -2,4 +2,4 @@
 IF NOT DEFINED QHOME	SET "QHOME=%CONDA_PREFIX%\kdb"
 IF NOT DEFINED QLIC	SET "QLIC=%HOMEDRIVE%%HOMEPATH%"
 
-IF NOT EXIST k4.lic IF NOT EXIST "%QLIC%\k4.lic" IF NOT EXIST "%QHOME%\k4.lic" IF NOT EXIST kc.lic IF NOT EXIST "%QLIC%\kc.lic" IF NOT EXIST "%QHOME%\kc.lic" python "%CONDA_PREFIX%\kdb\kc.lic.py" || echo 'q non-functional, to re-run the license fetcher reactivate your env' >&2
+IF NOT EXIST k4.lic IF NOT EXIST "%QLIC%\k4.lic" IF NOT EXIST "%QHOME%\k4.lic" IF NOT EXIST kc.lic IF NOT EXIST "%QLIC%\kc.lic" IF NOT EXIST "%QHOME%\kc.lic" python "%CONDA_PREFIX%\kdb\kc.lic.py" || echo q non-functional, to re-run the license fetcher reactivate your env >&2
--- conda/activate.sh
+++ conda/activate.sh
@@ -4,4 +4,4 @@
 [ -f k4.lic ] || [ -f "${QLIC}/k4.lic" ] || [ -f "{QHOME}/k4.lic" ] \
 	|| [ -f kc.lic ] || [ -f "${QLIC}/kc.lic" ] || [ -f "{QHOME}/kc.lic" ] \
 	|| python "${CONDA_PREFIX}/kdb/kc.lic.py" \
-	|| echo 'q non-functional, to re-run the license fetcher reactivate your env' >&2
+	|| echo q non-functional, to re-run the license fetcher reactivate your env >&2
